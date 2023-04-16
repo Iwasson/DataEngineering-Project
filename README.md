99ee3e3631aa87d24a358af592fb7e8b1c9c13e5
dev <kdugas@pdx.edu>
2023-04-16 12:45:48 -0700

    Initial producer-consumer workflow
    Produce and consume single row at a time
    
    Added monitoring and logging level control
    
    Fix indentation from default kafka scripts
    
    Add doc comments
    
    Abstracted kafka functionality to api file
    
    Abstract kafka functionality to api file
    
    Create snapshot dir if it doesn't exist
    
    Remove flush_topic script, add --flush arg instead
    
    Add type hints and doc comments
    
    Add snapshots folder to version control, remove check if folder exists
    
    Change file name
    
    Add log message
    
    Updating storage solution
    
    Update kafka_api.py

---
+++ README.md
@@ -0,0 +1,10 @@
+# No Data 'Bout It
+
+## Resources
+
+Temporary link farm
+
+- [Create a linux vm in Compute Engine](https://cloud.google.com/compute/docs/create-linux-vm-instance)
+- [Kafka setup tutorial](https://developer.confluent.io/get-started/python/#kafka-setup)
+- [confluent-kafka repo](https://github.com/confluentinc/confluent-kafka-python)
+- [Kafka API documentation](https://docs.confluent.io/home/overview.html#confluent-documentation)
--- transport/snapshot.py
+++ transport/snapshot.py
@@ -10,7 +10,7 @@ import json
 import requests
 import sqlite3
 from loguru import logger
-from typing import List
+from typing import List, Tuple
 
 def download_data(url: str) -> json:
   """
@@ -23,40 +23,54 @@ def download_data(url: str) -> json:
     return data.json()
   except json.JSONDecodeError:
       logger.warning(f"Error Getting URL: {url}")
+      logger.warning(f"Response: {data.text}")
       sys.exit(1)
    
 
-def parse_data(data: json) -> List[tuple]:
+def parse_data(data: json) -> List[tuple] :
   """
   Takes in the json data from the website and converts it
-  into usable data for insertion into a DB
+  into usable data for insertion into a DB and Kafka
 
   Returns a List of tuples
   """
-  parsed_data = []
+  
+  sql_data = []
   for row in data:
-     cols = list(row.values())
-     for index, col in enumerate(cols):
-        if col == "":
-           cols[index] = 0
+    tupe = sql_parse(row)
+    sql_data.append(tupe)
 
-     tupe = (int(cols[0]),
-            int(cols[1]),
-            str(cols[2]),
-            int(cols[3]),
-            int(cols[4]),
-            int(cols[5]),
-            float(cols[6]),
-            float(cols[7]),
-            float(cols[8]),
-            float(cols[9]))
-     parsed_data.append(tupe)
-  return parsed_data
+  return sql_data
 
-def store_data(data: json) -> None:
+
+def sql_parse(row: dict) -> Tuple:
+  """
+  Convert non-string fields to the appropriate type and
+  set empty fields to 0.
+
+  Returns a tuple
+  """
+
+  cols = list(row.values())
+  for index, col in enumerate(cols):
+    if col == "":
+        cols[index] = 0
+
+  return (int(cols[0]),
+        int(cols[1]),
+        str(cols[2]),
+        int(cols[3]),
+        int(cols[4]),
+        int(cols[5]),
+        float(cols[6]),
+        float(cols[7]),
+        float(cols[8]),
+        float(cols[9]))
+
+
+def store_data(parsed_data: json) -> None:
   """
   Takes in a json object and stores that data in a sqlite3 DB
-
   Returns None
   """
   conn = sqlite3.connect("trimet.db")
@@ -90,19 +104,19 @@ def store_data(data: json) -> None:
   VALUES(?,?,?,?,?,?,?,?,?,?)
   """
 
-  parsed_data = parse_data(data)
-
   for row in parsed_data:
      cur.execute(insert_sql, row)
   conn.commit()
 
-def main():
+
+def get_snapshot():
   url = "http://www.psudataeng.com:8000/getBreadCrumbData"
   logger.info(f"Getting data from: {url}")
   data = download_data(url)
   logger.info(f"Got {len(data)} number of entries...")
-  logger.info("Storing data...")
-  store_data(data)
-  logger.info("Done!")
-
-main()
+  logger.info("Parsing data...")
+  #sql_data = parse_data(data)
+  #logger.info("Storing data in trimet.db...")
+  #store_data(sql_data)
+  logger.info(f"Done!")
+  return data
