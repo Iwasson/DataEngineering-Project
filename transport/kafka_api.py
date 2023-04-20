60431ad2ffa799da317fafbf87ec334851da281f
dev <kdugas@pdx.edu>
2023-04-19 17:29:06 -0700

    Abstract kafka functionality to api file

--- transport/kafka_api.py
+++ transport/kafka_api.py
@@ -42,10 +42,6 @@ def consume_events(topic, consumer, logger):
   while True:
     msg = consumer.poll(1.0)
     if msg is None:
-      # if len(data) > 0:
-      #   logger.info(f'Finished consuming events from {topic}. Storing data...')
-      #   break
-
       # Initial message consumption may take up to
       # `session.timeout.ms` for the consumer group to
       # rebalance and start consuming
